913dcc1f24cea8ff73110af07fec11124f0a559e
avyfain <avyfain@gmail.com>
2016-06-15 22:01:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# A Link Please
+
+[@fmanjoo](https://twitter.com/fmanjoo) asked for a link please, [@hunterwalk](https://twitter.com/hunterwalk) then [replied](https://twitter.com/fmanjoo/status/743111026493689856).
+
+Everyone should have links, please.
+
+![Link](/img/5.png?raw=true)
---
+++ tweet.py
@@ -0,0 +1,27 @@
+import os
+import random
+import tweepy
+
+CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+phrases = ['Here you go',
+           'A link coming right up',
+           'Done',
+           "I know what you're thinking. Zelda was the princess though"]
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+twitter = tweepy.API(auth)
+
+
+if __name__ == '__main__':
+    random_tweet = twitter.search('"link please"')[0]
+    username = random_tweet.user.screen_name
+
+    img = 'img/{}.png'.format(random.choice(range(10)))
+    message = '{}, @{}!'.format(random.choice(phrases), username)
+
+    twitter.update_with_media(filename=img, status=message)
