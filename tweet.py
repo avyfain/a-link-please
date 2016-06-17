d9bb1c6fbe366205b2dd9a79761ab67ba9ff63f3
avyfain <avyfain@gmail.com>
2016-06-17 00:19:46 -0700

    answer to replies

--- tweet.py
+++ tweet.py
@@ -1,11 +1,14 @@
 import os
 import random
 import tweepy
+import redis
 
 CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 phrases = ['Here you go',
            'A link coming right up',
@@ -16,14 +19,23 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 twitter = tweepy.API(auth)
 
-
-if __name__ == '__main__':
-    random_tweet = twitter.search('"link please"')[0]
-    username = random_tweet.user.screen_name
+def tweet_back(tweet):
+    username = tweet.user.screen_name
 
     img = 'img/{}.png'.format(random.choice(range(10)))
     message = '{}, @{}!'.format(random.choice(phrases), username)
 
-    print("Replying to {}'s tweet with ID {}".format(username, random_tweet.id))
+    print("Replying to {}'s tweet with ID {}".format(username, tweet.id))
+    twitter.update_with_media(filename=img, status=message, in_reply_to_status_id=tweet.id)
 
-    twitter.update_with_media(filename=img, status=message, in_reply_to_status_id=random_tweet.id)
+if __name__ == '__main__':
+    tweets = twitter.search('"link please"')
+    random_tweet = next((tweet for tweet in tweets if not tweet.retweeted), None)
+
+    tweet_back(random_tweet)
+
+    replies = twitter.search('@alinkplease link please', since_id=r.get('last'))
+    if replies:
+        r.set('last', replies[0].id)
+    for tweet in replies:
+        tweet_back(tweet)
